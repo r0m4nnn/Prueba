e2e1d8ac376088ccfe3593fabc9c346b5956fc1e
Roman Arroyo <arroyoroman32@gmail.com>
2023-07-08 16:58:19 -0300

    primer commit

---
+++ ejercicio_1_java.js
@@ -0,0 +1,27 @@
+let resultado
+function sumar(){
+    var x = parseInt(document.getElementById('Valor1').value)
+    var y = parseInt(document.getElementById('valor2').value)
+   const resultado = x+y ;
+   alert(resultado)
+}
+function restar(){
+    var x = parseInt(document.getElementById('Valor1').value)
+    var y = parseInt(document.getElementById('valor2').value)
+    const resultado = x-y ;
+    alert(resultado);
+
+}
+function dividir(){
+    var x = parseInt(document.getElementById('Valor1').value)
+    var y = parseInt(document.getElementById('valor2').value)
+    const resultado = x/y ;
+   alert(resultado) ;
+}
+function multiplicar(){
+    var x = parseInt(document.getElementById('Valor1').value)
+    var y = parseInt(document.getElementById('valor2').value)
+    const resultado = x*y ;
+   alert(resultado) ;
+
+}
